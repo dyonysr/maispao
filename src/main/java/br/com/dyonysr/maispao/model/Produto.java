bb56ec2b8509a8596b56696a7537d7dcc5942b66
dyony <dyonyrodrigues@gmail.com>
2023-06-24 17:54:36 -0300

    commit Criação Classe Produto

---
+++ src/main/java/br/com/dyonysr/maispao/model/Produto.java
@@ -0,0 +1,41 @@
+package br.com.dyonysr.maispao.model;
+
+public class Produto {
+    private Long id;
+    private String nome;
+    private double preco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    private int quantidade;
+}
