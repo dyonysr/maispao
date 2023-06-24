1ae6a0af69931f97796898df7b8bcafd50380386
dyony <dyonyrodrigues@gmail.com>
2023-06-24 16:31:47 -0300

    commit inicial que criou a aplicacao

---
+++ src/main/java/br/com/dyonysr/maispao/MaispaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dyonysr.maispao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaispaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaispaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dyonysr/maispao/MaispaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dyonysr.maispao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaispaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
